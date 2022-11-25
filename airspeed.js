d3800f2aff2a430474ff2f048217670500a7b41e
huikml <67998517+huikml@users.noreply.github.com>
2022-11-25 10:22:57 +0800

    Update airspeed.js

--- airspeed.js
+++ airspeed.js
@@ -1,18 +1,26 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+Airspeed
+脚本名称:Airspeed
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
 [rewrite_local]
-^https?:\/\/api\.express-api\.com\/v1\/connect.+$ url script-response-body https://raw.githubusercontent.com/huikml/js/main/airspeed.js
+^http[s]?:\/\/api.express-api.com\/v1\/device.+$ url script-response-body express-api.js
 [mitm] 
-hostname = api.express-api.com
+hostname = *.express-api.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/api.express-api.com\/v1\/connect.+$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/huikml/js/main/airspeed.js
+^http[s]?:\/\/api.express-api.com\/v1\/device.+$ requires-body=1,max-size=0,script-path=express-api.js
 
 [MITM]
-hostname = api.express-api.com
+hostname = *.express-api.*
 
 *******************************/
-
 var obj = JSON.parse($response.body);
     obj.data.type= "gold;
 obj.data.username= "jumnyliu;
