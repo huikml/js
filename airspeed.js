57260960af43af39edb757e0aab8db6dd41b1485
huikml <67998517+huikml@users.noreply.github.com>
2023-05-06 19:00:48 +0800

    Update airspeed.js

--- airspeed.js
+++ airspeed.js
@@ -1,28 +1,39 @@
-/*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-Airspeed
-脚本名称:Airspeed
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
-群1077223830
+/******************************
+
+脚本功能：Air speed vpn 黄金VIP
+软件版本：3.8.6
+脚本作者：jumny liu
+更新时间：2023-5-6
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
 *******************************
+
 [rewrite_local]
-^http[s]?:\/\/api.express-api.com\/v1\/device.+$ url script-response-body https://raw.githubusercontent.com/huikml/js/main/airspeed.js
+
+^https://api\.express-api\.com/v1/device url script-response-body airspeed.js
+^https://api\.express-api\.com/v1/connect url script-response-body airspeed.js
+
 [mitm] 
 hostname = api.express-api.com
-*******************************
-Surge
 
-[Script]
-^http[s]?:\/\/api.express-api.com\/v1\/device.+$ requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/huikml/js/main/airspeed.js
-
-[MITM]
-hostname = api.express-api.com
 
 *******************************/
-var obj = JSON.parse($response.body);
-    obj.data.type= "gold;
-obj.data.username= "jumnyliu;
-obj.data.expire= 9648718129884;
-    $done({body: JSON.stringify(obj)});
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (obj.type) {
+  obj.type = 'gold';
+}
+
+if (obj.username) {
+  obj.username = 'jumnyliu';
+}
+
+if (obj.expire) {
+  obj.expire = 9648718129884;
+}
+
+body = JSON.stringify(obj);
+$done({ body });
