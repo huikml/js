4af3add049507bf53dce219512e0dcdabdf519db
huikml <67998517+huikml@users.noreply.github.com>
2023-10-20 02:04:08 +0800

    Create qa.js

---
+++ qa.js
@@ -0,0 +1,10 @@
+# NAME: qatarairways.com
+# AUTHOR: 
+# REPO: 
+# UPDATED: 2023-10-20 14:23:16
+# HOST-KEYWORD: 
+# HOST-SUFFIX: 3
+# TOTAL: 3
+HOST-SUFFIX,qatarairways.com,qatar
+HOST-SUFFIX,sieintercept.qualrics.com,qatar
+HOST-SUFFIX,moi.gov.qa,qatar
